c6f7d574c6cf08eb5a02fdfef4b469becd324abc
Catalin Adler <catalin.adler@devfactory.com>
2020-08-18 22:12:48 +0200

    silencing some logs.

--- src/MySqlConnector/MySqlConnection.cs
+++ src/MySqlConnector/MySqlConnection.cs
@@ -664,8 +664,8 @@ namespace MySqlConnector
 		{
 			try
 			{
-				Log.Info("Trying to cancel command {0} having timeout of {1}s executing since {2}.",
-					command switch { MySqlCommand cmd => cmd.CommandText, _ => string.Empty }, command.CommandTimeout, DateTimeOffset.UtcNow - command.ExecutingSince);
+				Log.Info("Trying to cancel command {0} {1} having an active reader and a timeout of {2}s executing since {3}.",
+					command switch { MySqlCommand cmd => cmd.CommandText, _ => string.Empty }, HasActiveReader ? string.Empty : "not", command.CommandTimeout, DateTimeOffset.UtcNow - command.ExecutingSince);
 
 				var session = Session;
 				if (!session.TryStartCancel(command))
